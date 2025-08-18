0a3086c1a20e32051f78facd8a4f0d2edb69d072
405091-GUZMAN <405091@tecnicatura.frc.utn.edu.ar>
2025-08-18 17:45:20 -0300

    Estructura backend v0.1

---
+++ backend/SalesDashboard/src/main/java/guzman/SalesDashboard/SalesDashboardApplication.java
@@ -0,0 +1,13 @@
+package guzman.SalesDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/SalesDashboard/src/test/java/guzman/SalesDashboard/SalesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package guzman.SalesDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
